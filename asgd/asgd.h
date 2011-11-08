7ae6bcf0f0d93e88b9a55ea91db23ddc54c397a6
Claudio A Andreoni <cloudguitar@gmail.com>
2011-11-08 03:59:26 -0500

    correct parameters for BLAS calls

---
+++ asgd/asgd.h
@@ -0,0 +1,83 @@
+#ifndef _ASGD_H_
+#define _ASGD_H_
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct matrix matrix_t;
+struct matrix {
+	size_t rows;
+	size_t cols;
+	float *data;
+};
+
+matrix_t *matrix_init(size_t rows, size_t cols, float val);
+
+void matrix_destr(matrix_t *m);
+
+void matrix_copy(matrix_t *dst, const matrix_t *src);
+
+matrix_t *matrix_clone(matrix_t *m);
+
+void matrix_swap(matrix_t *m, size_t j, size_t k, size_t x, size_t y);
+
+void matrix_row_shuffle(matrix_t *m);
+
+float matrix_get(matrix_t *m, size_t i, size_t j);
+
+void matrix_set(matrix_t *m, size_t i, size_t j, float val);
+
+float *matrix_row(matrix_t *m, size_t i);
+
+void mex_assert(bool cond, const char *mex);
+
+typedef struct nb_asgd nb_asgd_t;
+struct nb_asgd
+{
+	size_t n_feats;
+	size_t n_iters;
+	float l2_reg;
+	bool feedback;
+
+	matrix_t *sgd_weights;
+	matrix_t *sgd_bias;
+	float sgd_step_size;
+	float sgd_step_size0;
+
+	float sgd_step_size_scheduling_exp;
+	float sgd_step_size_scheduling_mul;
+
+	matrix_t *asgd_weights;
+	matrix_t *asgd_bias;
+	float asgd_step_size;
+	float asgd_step_size0;
+
+	uint64_t n_observs;
+};
+
+nb_asgd_t *nb_asgd_init(
+	uint64_t n_feats,
+	float sgd_step_size0,
+	float l2_reg,
+	uint64_t n_iters,
+	bool feedback);
+
+void nb_asgd_destr(
+		nb_asgd_t *data);
+
+void fit(nb_asgd_t *data, matrix_t *X, matrix_t *y);
+
+void partial_fit(nb_asgd_t *data, matrix_t *X, matrix_t *y);
+
+void decision_function(nb_asgd_t *data, matrix_t *X, matrix_t *r);
+
+void predict(matrix_t *r);
+
+
+#endif
+
---
+++ asgd/tests/asgd_unit.c
@@ -0,0 +1,252 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "../asgd.h"
+
+bool test_swap()
+{
+	float in[3][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}};
+	float out[3][5] = {{15,2,3,4,11}, {6,7,8,9,10}, {5,12,13,14,1}};
+	matrix_t *m = matrix_init(3, 5, 0.0f);
+	memcpy(m->data, in, 3*5*sizeof(*m->data));
+	matrix_swap(m, 0, 0, 2, 4);
+	matrix_swap(m, 0, 4, 2, 0);
+	bool res = !memcmp(m->data, out, 3*5*sizeof(*m->data));
+	matrix_destr(m);
+	if (res)
+	{
+		fprintf(stdout, "matrix_swap passed\n\n");
+		return true;
+	}
+	else
+	{
+		fprintf(stderr, "matrix_Fswap failed\n\n");
+		return false;
+	}
+}
+
+bool test_row_shuffle()
+{
+	float in[5][3] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15}};
+	matrix_t *m = matrix_init(5, 3, 0.0f);
+	memcpy(m->data, in, 5*3*sizeof(*m->data));
+	matrix_row_shuffle(m);
+	bool got_row0 = false;
+	bool got_row1 = false;
+	bool got_row2 = false;
+	bool got_row3 = false;
+	bool got_row4 = false;
+	for (size_t i = 0; i < 5; ++i)
+	{
+		if (!memcmp(m->data+i*3, in[0], 3)) got_row0 = true;
+		if (!memcmp(m->data+i*3, in[1], 3)) got_row1 = true;
+		if (!memcmp(m->data+i*3, in[2], 3)) got_row2 = true;
+		if (!memcmp(m->data+i*3, in[3], 3)) got_row3 = true;
+		if (!memcmp(m->data+i*3, in[4], 3)) got_row4 = true;
+	}
+	matrix_destr(m);
+
+	bool res = got_row0 && got_row1 && got_row2 && got_row3 && got_row4;
+	if (res)
+	{
+		fprintf(stdout, "durstenfeld_shuffle passed\n\n");
+		return true;
+	}
+	else
+	{
+		fprintf(stderr, "durstenfeld_shuffle failed\n\n");
+		return false;
+	}
+}
+
+bool test_partial_fit()
+{
+	float Xd[5][3] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15}};
+	matrix_t *Xm = matrix_init(5, 3, 0.0f);
+	memcpy(Xm->data, Xd, sizeof(Xd));
+
+	float yd[5][1] = {{16},{17},{18},{19},{20}};
+	matrix_t *ym = matrix_init(5, 1, 0.0f);
+	memcpy(ym->data, yd, sizeof(yd));
+
+	float out_weights[3][1] =
+	{
+		{0.016f}, {0.032f}, {0.048f}
+	};
+	float out_bias[1][1] = {{0.016f}};
+
+	nb_asgd_t *clf = nb_asgd_init(3, 1e-3f, 1e-6f, 4, false);
+	partial_fit(clf, Xm, ym);
+
+	bool res = true;
+	printf("testing partial_fit\n");
+	for (size_t i = 0; i < 3; ++i)
+	{
+		for (size_t j = 0; j < 1; ++j)
+		{
+			printf("sgd_weights %zu %zu exp: %f got: %f\n",
+				i, j,
+				out_weights[i][j],
+				matrix_get(clf->sgd_weights, i, j));
+			if(fabs(matrix_get(clf->sgd_weights, i, j) - out_weights[i][j]) > 1e-5)
+			{
+				res = false;
+			}
+		}
+	}
+
+	printf("sgd_bias exp: %f got: %f\n", out_bias[0][0], clf->sgd_bias->data[0]);
+	if (fabs(clf->sgd_bias->data[0] - out_bias[0][0]) > 1e-5)
+	{
+		res = false;
+	}
+
+	printf("asgd_bias exp: %f got: %f\n", out_bias[0][0], clf->asgd_bias->data[0]);
+	if (fabs(clf->asgd_bias->data[0] - out_bias[0][0]) > 1e-5)
+	{
+		res = false;
+	}
+
+
+	nb_asgd_destr(clf);
+	matrix_destr(Xm);
+	matrix_destr(ym);
+
+	if (res)
+	{
+		fprintf(stdout, "partial_fit passed\n\n");
+		return true;
+	}
+	else
+	{
+		fprintf(stderr, "partial_fit failed\n\n");
+		return false;
+	}
+}
+
+bool test_decision_function()
+{
+	float Xd[5][3] = {{1,2,3},{4,5,6},{7,8,-9},{10,11,12},{13,14,15}};
+	matrix_t *Xm = matrix_init(5, 3, 0.0f);
+	memcpy(Xm->data, Xd, sizeof(Xd));
+
+	float yd[5][1] = {{16},{17},{18},{19},{20}};
+	matrix_t *ym = matrix_init(5, 1, 0.0f);
+	memcpy(ym->data, yd, sizeof(yd));
+
+	matrix_t *rm = matrix_init(5, 1, 0.0f);
+
+	float in_weights[3][1] =
+	{
+		{0.016f}, {0.032f}, {0.048f}
+	};
+	float in_bias[1][1] = {{0.016f}};
+	float out_r[5][1] = {{0.24f}, {0.528f}, {-0.0479999f}, {1.104f}, {1.392f}};
+
+	nb_asgd_t *clf = nb_asgd_init(3, 1e-3f, 1e-6f, 4, false);
+	memcpy(clf->asgd_weights->data, in_weights, sizeof(in_weights));
+	memcpy(clf->asgd_bias->data, in_bias, sizeof(in_bias));
+
+	decision_function(clf, Xm, rm);
+	bool res = true;
+	printf("testing decision_function\n");
+	for (size_t i = 0; i < 1; ++i)
+	{
+		for (size_t j = 0; j < 5; ++j)
+		{
+			printf("exp: %f got: %f\n",
+				out_r[i][j],
+				matrix_get(rm, i, j));
+			if(fabs(matrix_get(rm, i, j) - out_r[i][j]) > 1e-5)
+			{
+				res = false;
+			}
+		}
+	}
+
+	nb_asgd_destr(clf);
+	matrix_destr(Xm);
+	matrix_destr(ym);
+	matrix_destr(rm);
+
+	if (res)
+	{
+		fprintf(stdout, "decision_function passed\n\n");
+		return true;
+	}
+	else
+	{
+		fprintf(stderr, "decision_function failed\n\n");
+		return false;
+	}
+}
+
+bool test_predict()
+{
+	float in_r[5][5] = {
+		{0.4324, -0.0297, 243.0920, -98389, 8939843},
+		{0.5405, 5489.459, 9845.845, 454.25, -858.954},
+		{343.53, 9345.98, 4.5954, 98.455, -80.934},
+		{9630.49, 98.2304, -34.0495, 10974.5, 8.7457},
+		{-350.785, 30.73407, -14.353, -34.0340, 983.453}};
+	matrix_t *mr = matrix_init(5, 5, 0.0f);
+	memcpy(mr->data, in_r, sizeof(in_r));
+	float out_r[5][5] = {
+		{1.0, -1.0, 1.0, -1, 1},
+		{1, 1, 1, 1, -1},
+		{1, 1, 1, 1, -1},
+		{1, 1, -1, 1, 1},
+		{-1, 1, -1, -1, 1}};
+
+	predict(mr);
+
+	printf("testing predict\n");
+	bool res = true;
+	for (size_t i = 0; i < mr->rows; ++i)
+	{
+		for (size_t j = 0; j < mr->cols; ++j)
+		{
+			printf("exp: %f got: %f\n",
+				out_r[i][j],
+				matrix_get(mr, i, j));
+			if (fabs(matrix_get(mr, i, j) - out_r[i][j]) > 1e-5)
+			{
+				res = false;
+			}
+		}
+	}
+
+	matrix_destr(mr);
+	if (res)
+	{
+		fprintf(stdout, "predict passed\n\n");
+		return true;
+	}
+	else
+	{
+		fprintf(stderr, "predict failed\n\n");
+		return false;
+	}
+}
+
+int main(void)
+{
+	int res = true;
+	res &= test_swap();
+	res &= test_row_shuffle();
+	res &= test_decision_function();
+	res &= test_predict();
+	res &= test_partial_fit();
+
+	if (res)
+	{
+		fprintf(stderr, "test passed\n");
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+		fprintf(stderr, "test failed\n");
+		return EXIT_FAILURE;
+	}
+}
+
