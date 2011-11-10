6add0dbf1ec15ef7081a920dd1e81d6616170d6f
Nicolas Pinto <pinto@mit.edu>
2011-11-10 03:05:23 -0500

    ENH: refactor/update/fix OVA and associated tests, add RandomState functionality, more error checking

--- asgd/naive_asgd.py
+++ asgd/naive_asgd.py
@@ -7,32 +7,74 @@ import numpy as np
 from numpy import dot
 from itertools import izip
 
+DEFAULT_SGD_STEP_SIZE0 = 1e-2
+DEFAULT_L2_REGULARIZATION = 1e-3
+DEFAULT_N_ITERATIONS = 10
+DEFAULT_FEEDBACK = False
+DEFAULT_RSTATE = None
+DEFAULT_DTYPE = np.float32
 
-class NaiveBinaryASGD(object):
-    def __init__(self, n_features, sgd_step_size0=1e-2, l2_regularization=1e-3,
-                 n_iterations=10, feedback=False, dtype=np.float32):
 
+class BaseASGD(object):
+
+    def __init__(self, n_features,
+                 sgd_step_size0=DEFAULT_SGD_STEP_SIZE0,
+                 l2_regularization=DEFAULT_L2_REGULARIZATION,
+                 n_iterations=DEFAULT_N_ITERATIONS, feedback=DEFAULT_FEEDBACK,
+                 rstate=DEFAULT_RSTATE, dtype=DEFAULT_DTYPE):
+
+        # --
+        assert n_features > 1
         self.n_features = n_features
+
+        assert n_iterations > 1
         self.n_iterations = n_iterations
+
         self.feedback = feedback
 
-        assert l2_regularization >= 0
+        if rstate is None:
+            rstate = np.random.RandomState()
+        self.rstate = rstate
+
+        assert l2_regularization > 0
         self.l2_regularization = l2_regularization
         self.dtype = dtype
 
-        self.sgd_weights = np.zeros((n_features), dtype=dtype)
-        self.sgd_bias = np.zeros((1), dtype=dtype)
+        # --
         self.sgd_step_size0 = sgd_step_size0
         self.sgd_step_size = sgd_step_size0
         self.sgd_step_size_scheduling_exponent = 2. / 3
         self.sgd_step_size_scheduling_multiplier = l2_regularization
 
-        self.asgd_weights = np.zeros((n_features), dtype=dtype)
-        self.asgd_bias = np.zeros((1), dtype=dtype)
         self.asgd_step_size0 = 1
         self.asgd_step_size = self.asgd_step_size0
 
         self.n_observations = 0
+
+
+class NaiveBinaryASGD(BaseASGD):
+
+    def __init__(self, n_features, sgd_step_size0=DEFAULT_SGD_STEP_SIZE0,
+                 l2_regularization=DEFAULT_L2_REGULARIZATION,
+                 n_iterations=DEFAULT_N_ITERATIONS, feedback=DEFAULT_FEEDBACK,
+                 rstate=DEFAULT_RSTATE, dtype=DEFAULT_DTYPE):
+
+        super(NaiveBinaryASGD, self).__init__(
+            n_features,
+            sgd_step_size0=sgd_step_size0,
+            l2_regularization=l2_regularization,
+            n_iterations=n_iterations,
+            feedback=feedback,
+            rstate=rstate,
+            dtype=dtype,
+            )
+
+        # --
+        self.sgd_weights = np.zeros((n_features), dtype=dtype)
+        self.sgd_bias = np.zeros((1), dtype=dtype)
+
+        self.asgd_weights = np.zeros((n_features), dtype=dtype)
+        self.asgd_bias = np.zeros((1), dtype=dtype)
 
     def partial_fit(self, X, y):
 
@@ -93,6 +135,8 @@ class NaiveBinaryASGD(object):
 
         self.n_observations = n_observations
 
+        return self
+
     def fit(self, X, y):
 
         assert X.ndim == 2
@@ -106,7 +150,7 @@ class NaiveBinaryASGD(object):
 
         for i in xrange(n_iterations):
 
-            idx = np.random.permutation(n_points)
+            idx = self.rstate.permutation(n_points)
             Xb = X[idx]
             yb = y[idx]
             self.partial_fit(Xb, yb)
@@ -115,6 +159,8 @@ class NaiveBinaryASGD(object):
                 self.sgd_weights = self.asgd_weights
                 self.sgd_bias = self.asgd_bias
 
+        return self
+
     def decision_function(self, X):
         return dot(self.asgd_weights, X.T) + self.asgd_bias
 
@@ -122,36 +168,40 @@ class NaiveBinaryASGD(object):
         return np.sign(self.decision_function(X))
 
 
-class NaiveOVAASGD(object):
+class NaiveOVAASGD(BaseASGD):
 
-    def __init__(self, n_classes, n_features, sgd_step_size0=1e-2,
-                 l2_regularization=1e-3, n_iterations=10, feedback=False,
-                 dtype=np.float32):
+    def __init__(self, n_classes, n_features,
+                 sgd_step_size0=DEFAULT_SGD_STEP_SIZE0,
+                 l2_regularization=DEFAULT_L2_REGULARIZATION,
+                 n_iterations=DEFAULT_N_ITERATIONS,
+                 feedback=DEFAULT_FEEDBACK,
+                 rstate=DEFAULT_RSTATE,
+                 dtype=DEFAULT_DTYPE):
 
+        super(NaiveOVAASGD, self).__init__(
+            n_features,
+            sgd_step_size0=sgd_step_size0,
+            l2_regularization=l2_regularization,
+            n_iterations=n_iterations,
+            feedback=feedback,
+            rstate=rstate,
+            dtype=dtype,
+            )
+
+        # --
+        assert n_classes > 1
         self.n_classes = n_classes
-        self.n_features = n_features
-        self.n_iterations = n_iterations
-        self.feedback = feedback
 
-        assert l2_regularization > 0
-        self.l2_regularization = l2_regularization
-        self.dtype = dtype
-
+        # --
         self.sgd_weights = np.zeros((n_features, n_classes), dtype=dtype)
         self.sgd_bias = np.zeros((n_classes,), dtype=dtype)
-        self.sgd_step_size0 = sgd_step_size0
-        self.sgd_step_size = sgd_step_size0
-        self.sgd_step_size_scheduling_exponent = 2. / 3
-        self.sgd_step_size_scheduling_multiplier = l2_regularization
-
         self.asgd_weights = np.zeros((n_features, n_classes), dtype=dtype)
         self.asgd_bias = np.zeros((n_classes), dtype=dtype)
-        self.asgd_step_size0 = 1
-        self.asgd_step_size = self.asgd_step_size0
-
-        self.n_observations = 0
 
     def partial_fit(self, X, y):
+
+        if set(y) > set(range(self.n_classes)):
+            raise ValueError("Invalid 'y'")
 
         sgd_step_size0 = self.sgd_step_size0
         sgd_step_size = self.sgd_step_size
@@ -216,11 +266,12 @@ class NaiveOVAASGD(object):
 
         self.n_observations = n_observations
 
+        return self
+
     def fit(self, X, y):
 
         assert X.ndim == 2
         assert y.ndim == 1
-        assert set(y) <= set(range(self.n_classes))
 
         n_points, n_features = X.shape
         assert n_features == self.n_features
@@ -230,7 +281,7 @@ class NaiveOVAASGD(object):
 
         for i in xrange(n_iterations):
 
-            idx = np.random.permutation(n_points)
+            idx = self.rstate.permutation(n_points)
             Xb = X[idx]
             yb = y[idx]
             self.partial_fit(Xb, yb)
@@ -238,6 +289,8 @@ class NaiveOVAASGD(object):
             if self.feedback:
                 self.sgd_weights = self.asgd_weights
                 self.sgd_bias = self.asgd_bias
+
+        return self
 
     def decision_function(self, X):
         return dot(X, self.asgd_weights) + self.asgd_bias
--- asgd/tests/test_naive_asgd.py
+++ asgd/tests/test_naive_asgd.py
@@ -1,8 +1,9 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, raises
 from numpy.testing import assert_allclose
 import numpy as np
+from numpy.random import RandomState
 
-from asgd import NaiveBinaryASGD as ASGD
+from asgd import NaiveBinaryASGD as BinaryASGD
 from asgd import NaiveOVAASGD as OVAASGD
 
 RTOL = 1e-6
@@ -11,132 +12,163 @@ ATOL = 1e-6
 N_POINTS = 1e3
 N_FEATURES = 1e2
 
-def get_fake_data(n_points, n_features, rseed):
-    np.random.seed(rseed)
-    X = np.random.randn(n_points, n_features).astype(np.float32)
-    y = 2 * (np.random.randn(n_points) > 0) - 1
+DEFAULT_ARGS = (N_FEATURES,)
+DEFAULT_KWARGS = dict(sgd_step_size0=1e-3,
+                      l2_regularization=1e-6,
+                      n_iterations=4,
+                      dtype=np.float32)
+
+
+def get_fake_data(n_points, n_features, rstate):
+    X = rstate.randn(n_points, n_features).astype(np.float32)
+    y = 2 * (rstate.randn(n_points) > 0) - 1
     X[y == 1] += 1e-1
     return X, y
 
 
-def get_fake_binary_data_multi_labels(n_points, n_features, rseed):
-    np.random.seed(rseed)
-    X = np.random.randn(n_points, n_features).astype(np.float32)
-    y = np.random.randn(n_points) > 0
+def get_fake_binary_data_multi_labels(n_points, n_features, rstate):
+    X = rstate.randn(n_points, n_features).astype(np.float32)
+    y = rstate.randn(n_points) > 0
     X[y] += 1e-1
     return X, y
 
 
-def get_fake_multiclass_data(n_points, n_features, n_classes, rseed):
-    np.random.seed(rseed)
-    X = np.random.randn(n_points, n_features).astype(np.float32)
-    z = np.random.random((n_points,))
-    y = np.zeros((n_points,))
-    for ind in range(n_classes):
-        I = (z < float(ind + 1) / n_classes) & (z >= float(ind) / n_classes)
-        y[I] = ind
+def get_fake_multiclass_data(n_points, n_features, n_classes, rstate):
+    X = rstate.randn(n_points, n_features).astype(np.float32)
+    y = rstate.randint(n_classes, size=n_points)
     return X, y
 
 
 def test_naive_asgd():
-    X, y = get_fake_data(N_POINTS, N_FEATURES, 42)
-    Xtst, ytst = get_fake_data(N_POINTS, N_FEATURES, 43)
 
-    clf = ASGD(N_FEATURES, sgd_step_size0=1e-3, l2_regularization=1e-6,
-               n_iterations=4, dtype=np.float32)
+    rstate = RandomState(42)
+
+    X, y = get_fake_data(N_POINTS, N_FEATURES, rstate)
+    Xtst, ytst = get_fake_data(N_POINTS, N_FEATURES, rstate)
+
+    clf = BinaryASGD(*DEFAULT_ARGS, rstate=rstate, **DEFAULT_KWARGS)
+
     clf.fit(X, y)
     ytrn_preds = clf.predict(X)
     ytst_preds = clf.predict(Xtst)
     ytrn_acc = (ytrn_preds == y).mean()
     ytst_acc = (ytst_preds == y).mean()
-    assert_equal(ytrn_acc, 0.723)
-    assert_equal(ytst_acc, 0.513)
+    assert_equal(ytrn_acc, 0.72)
+    assert_equal(ytst_acc, 0.522)
 
 
 def test_naive_asgd_with_feedback():
-    X, y = get_fake_data(N_POINTS, N_FEATURES, 42)
-    Xtst, ytst = get_fake_data(N_POINTS, N_FEATURES, 43)
 
-    clf = ASGD(N_FEATURES, sgd_step_size0=1e-3, l2_regularization=1e-6,
-               n_iterations=4, feedback=True, dtype=np.float32)
+    rstate = RandomState(43)
+
+    X, y = get_fake_data(N_POINTS, N_FEATURES, rstate)
+    Xtst, ytst = get_fake_data(N_POINTS, N_FEATURES, rstate)
+
+    clf = BinaryASGD(*DEFAULT_ARGS,
+                     feedback=True, rstate=rstate, **DEFAULT_KWARGS)
     clf.fit(X, y)
     ytrn_preds = clf.predict(X)
     ytst_preds = clf.predict(Xtst)
     ytrn_acc = (ytrn_preds == y).mean()
     ytst_acc = (ytst_preds == y).mean()
-    assert_equal(ytrn_acc, 0.707)
-    assert_equal(ytst_acc, 0.505)
+    assert_equal(ytrn_acc, 0.709)
+    assert_equal(ytst_acc, 0.449)
 
 
 def test_naive_asgd_multi_labels():
-    X, y = get_fake_binary_data_multi_labels(N_POINTS, N_FEATURES, 42)
-    Xtst, ytst = get_fake_binary_data_multi_labels(N_POINTS, N_FEATURES, 43)
+
+    rstate = RandomState(44)
+
+    X, y = get_fake_binary_data_multi_labels(N_POINTS, N_FEATURES, rstate)
+    Xtst, ytst = get_fake_binary_data_multi_labels(N_POINTS, N_FEATURES,
+                                                   rstate)
 
     # n_classes is 2 since it is actually a binary case
-    clf = OVAASGD(2, N_FEATURES, sgd_step_size0=1e-3,
-                  l2_regularization=1e-6, n_iterations=4, dtype=np.float32)
+    clf = OVAASGD(2, N_FEATURES, sgd_step_size0=1e-3, l2_regularization=1e-6,
+                  n_iterations=4, dtype=np.float32, rstate=rstate)
     clf.fit(X, y)
     ytrn_preds = clf.predict(X)
     ytst_preds = clf.predict(Xtst)
     ytrn_acc = (ytrn_preds == y).mean()
     ytst_acc = (ytst_preds == y).mean()
-    assert_equal(ytrn_acc, 0.723)
-    assert_equal(ytst_acc, 0.513)
+    assert_equal(ytrn_acc, 0.728)
+    assert_equal(ytst_acc, 0.52)
 
 
 def test_naive_multiclass_ova_asgd():
 
+    rstate = RandomState(45)
+
     n_classes = 10
 
-    X, y = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes, 42)
-    Xtst, ytst = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes, 43)
+    X, y = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes, rstate)
+    Xtst, ytst = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes,
+                                          rstate)
 
     clf = OVAASGD(n_classes, N_FEATURES, sgd_step_size0=1e-3,
-                  l2_regularization=1e-6, n_iterations=4, dtype=np.float32)
+                  l2_regularization=1e-6, n_iterations=4, dtype=np.float32,
+                  rstate=rstate)
+
     clf.fit(X, y)
+
     ytrn_preds = clf.predict(X)
     ytst_preds = clf.predict(Xtst)
     ytrn_acc = (ytrn_preds == y).mean()
     ytst_acc = (ytst_preds == y).mean()
-    assert_equal(ytrn_acc, 0.364)
-    assert_equal(ytst_acc, 0.116)
+
+    assert_equal(ytrn_acc, 0.335)
+    assert_equal(ytst_acc, 0.092)
 
 
 def test_naive_multiclass_ova_vs_binary_asgd():
 
+    rstate = RandomState(42)
+
     n_classes = 3
 
-    Xtrn, ytrn = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes, 42)
-    Xtst, ytst = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes, 43)
+    Xtrn, ytrn = get_fake_multiclass_data(
+        N_POINTS, N_FEATURES, n_classes, rstate)
+    Xtst, ytst = get_fake_multiclass_data(
+        N_POINTS, N_FEATURES, n_classes, rstate)
 
-    args = (N_FEATURES,)
-    kwargs = dict(sgd_step_size0=1e-3,
-                  l2_regularization=1e-6,
-                  n_iterations=4,
-                  dtype=np.float32)
+    # -- ground truth 'gt': emulate OVA with binary asgd classifiers
+    # binary assignments
+    ytrns = ytrn[np.newaxis, :] == np.arange(n_classes)[:, np.newaxis]
+    # transform into -1 / +1
+    ytrns = 2 * ytrns - 1
+    # get individual predictions
+    basgds = [
+        BinaryASGD(
+            *DEFAULT_ARGS,
+            rstate=RandomState(999),
+            **DEFAULT_KWARGS).partial_fit(Xtrn, ysel)
+        for ysel in ytrns
+    ]
+    ytsts = [basgd.decision_function(Xtst) for basgd in basgds]
+    # convert to array of shape (n_classes, n_points)
+    gt = np.array(ytsts).T
 
-    # -- ground truth 'gt'
-    # emulate OVA with binary asgd classifiers
-    ytrn0 = 2 * (ytrn == 0).astype(np.int) - 1
-    ytrn1 = 2 * (ytrn == 1).astype(np.int) - 1
-    ytrn2 = 2 * (ytrn == 2).astype(np.int) - 1
-
-    clf0 = ASGD(*args, **kwargs)
-    clf0.partial_fit(Xtrn, ytrn0)
-    clf1 = ASGD(*args, **kwargs)
-    clf1.partial_fit(Xtrn, ytrn1)
-    clf2 = ASGD(*args, **kwargs)
-    clf2.partial_fit(Xtrn, ytrn2)
-
-    m0 = clf0.decision_function(Xtst)
-    m1 = clf1.decision_function(Xtst)
-    m2 = clf2.decision_function(Xtst)
-
-    gt = np.column_stack([m0, m1, m2])
-
-    # -- given 'gv'
-    clf = OVAASGD(*((n_classes,) + args), **kwargs)
+    # -- given 'gv': use the OVA class
+    clf = OVAASGD(*((n_classes,) + DEFAULT_ARGS),
+                  rstate=RandomState(999), **DEFAULT_KWARGS)
     clf.partial_fit(Xtrn, ytrn)
     gv = clf.decision_function(Xtst)
 
+    # -- test
     assert_allclose(gv, gt, rtol=RTOL, atol=ATOL)
+
+
+@raises(ValueError)
+def test_naive_ova_asgd_wrong_labels():
+
+    rstate = RandomState(42)
+
+    n_classes = 10
+
+    Xtrn, ytrn = get_fake_multiclass_data(N_POINTS, N_FEATURES, n_classes,
+                                          rstate)
+
+    clf = OVAASGD(*((n_classes,) + DEFAULT_ARGS),
+                  rstate=RandomState(999), **DEFAULT_KWARGS)
+    ytrn_bad = rstate.randint(n_classes + 42, size=len(ytrn))
+    clf.partial_fit(Xtrn, ytrn_bad)
