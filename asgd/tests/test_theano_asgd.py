59a6adeee0b3b8159c859e92674a89b726e70a6b
James Bergstra <james.bergstra@gmail.com>
2012-01-17 18:06:20 -0500

    adding TheanoBinaryASGD and tests

---
+++ asgd/tests/test_theano_asgd.py
@@ -0,0 +1,113 @@
+import time
+from copy import copy
+
+from nose.tools import assert_equal, raises
+from nose.plugins.skip import SkipTest
+from numpy.testing import assert_allclose
+import numpy as np
+from numpy.random import RandomState
+
+from asgd.theano_asgd import TheanoBinaryASGD
+from asgd.naive_asgd import NaiveBinaryASGD
+from test_naive_asgd import get_fake_data
+
+RTOL = 1e-6
+ATOL = 1e-6
+
+N_POINTS = 1e3
+N_FEATURES = 1e2
+
+DEFAULT_ARGS = (N_FEATURES,)
+DEFAULT_KWARGS = dict(sgd_step_size0=1e-3,
+                      l2_regularization=1e-6,
+                      n_iterations=4,
+                      dtype=np.float32)
+
+
+
+def test_theano_binary_asgd_like_naive_asgd():
+
+    rstate = RandomState(42)
+
+    X, y = get_fake_data(N_POINTS, N_FEATURES, rstate)
+    Xtst, ytst = get_fake_data(N_POINTS, N_FEATURES, rstate)
+
+    clf0 = NaiveBinaryASGD(*DEFAULT_ARGS, rstate=copy(rstate), **DEFAULT_KWARGS)
+    clf1 = TheanoBinaryASGD(*DEFAULT_ARGS, rstate=copy(rstate), **DEFAULT_KWARGS)
+
+    for clf in [clf0, clf1]:
+        clf.fit(X, y)
+        ytrn_preds = clf.predict(X)
+        ytst_preds = clf.predict(Xtst)
+        ytrn_acc = (ytrn_preds == y).mean()
+        ytst_acc = (ytst_preds == y).mean()
+        assert_equal(ytrn_acc, 0.72)
+        assert_equal(ytst_acc, 0.522)
+
+
+def test_theano_binary_asgd_converges_to_truth():
+    n_features = 5
+
+    rstate = RandomState(42)
+
+    true_weights = rstate.randn(n_features)
+    true_bias = rstate.randn() * 0
+    clf = TheanoBinaryASGD(n_features,
+            rstate=rstate,
+            sgd_step_size0=0.1,
+            dtype='float64',
+            l2_regularization = 1e-4,
+            sgd_step_size_scheduling_exponent=0.5,
+            sgd_step_size_scheduling_multiplier=1.0)
+
+    Tmax = 300
+
+    eweights = np.zeros(Tmax)
+    ebias = np.zeros(Tmax)
+
+    for i in xrange(Tmax):
+        X = rstate.randn(20, 5)
+        labels = np.sign(np.dot(X, true_weights) + true_bias).astype('int')
+        # toss in some noise
+        labels[0:3] = -1
+        labels[3:6] = 1
+        clf.partial_fit(X, labels)
+        eweights[i] = 1 - np.dot(true_weights, clf.asgd_weights) / \
+                (np.sqrt((true_weights ** 2).sum() * (clf.asgd_weights **
+                    2).sum()))
+        ebias[i] = (true_bias - clf.asgd_bias)**2
+
+    if 0:
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(Tmax), eweights, label='weights cos')
+        plt.plot(np.arange(Tmax), ebias, label='bias sse')
+        plt.legend()
+        plt.show()
+    else:
+        assert eweights[50:].max() < 0.002
+        assert ebias[50:].max() < 0.005
+
+
+def run_theano_binary_asgd_speed():
+
+    rstate = RandomState(42)
+
+    X, y = get_fake_data(N_POINTS, N_FEATURES, rstate)
+    Xtst, ytst = get_fake_data(N_POINTS, N_FEATURES, rstate)
+
+    kwargs = dict(DEFAULT_KWARGS)
+    kwargs['n_iterations'] = 10
+
+    clf0 = NaiveBinaryASGD(*DEFAULT_ARGS, rstate=copy(rstate), **DEFAULT_KWARGS)
+    clf1 = TheanoBinaryASGD(*DEFAULT_ARGS, rstate=copy(rstate), **DEFAULT_KWARGS)
+    clf1.compile_train_fn_2()
+
+    t = time.time()
+    clf0.fit(X, y)
+    t0 = time.time() - t
+
+    t = time.time()
+    clf1.fit(X, y)
+    t1 = time.time() - t
+    print 'TIMES', 'Naive', t0, 'Theano', t1
+
---
+++ asgd/theano_asgd.py
@@ -0,0 +1,197 @@
+
+import numpy as np
+from itertools import izip
+
+from naive_asgd import BaseASGD
+
+from naive_asgd import (
+        DEFAULT_SGD_STEP_SIZE0,
+        DEFAULT_L2_REGULARIZATION,
+        DEFAULT_N_ITERATIONS,
+        DEFAULT_FEEDBACK,
+        DEFAULT_RSTATE,
+        DEFAULT_DTYPE,
+        DEFAULT_SGD_EXPONENT,
+        DEFAULT_SGD_TIMESCALE)
+
+import theano
+import theano.tensor as tensor
+
+class TheanoBinaryASGD(BaseASGD):
+
+    """
+    Notes regarding speed:
+    1. This could be sped up futher by implementing an sdot Op in e.g.
+       theano's blas_c.py file.  (currently _dot22 is used for inner product)
+
+    2. Algorithmically, http://www.dbs.ifi.lmu.de/~yu_k/cvpr11_0694.pdf
+    describes a change of variables that can potentially reduce the number of
+    updates required by the algorithm, but I think it returns the same
+    advantage as loop fusion / proper BLAS usage.
+    """
+
+    sgd_weights = property(lambda self: self.s_sgd_weights.get_value(),
+            lambda self, val: self.s_sgd_weights.set_value(val))
+
+    sgd_bias = property(lambda self: self.s_sgd_bias.get_value(),
+            lambda self, val: self.s_sgd_weights.set_value(val))
+
+    asgd_weights = property(lambda self: self.s_asgd_weights.get_value(),
+            lambda self, val: self.s_asgd_weights.set_value(val))
+
+    asgd_bias = property(lambda self: self.s_asgd_bias.get_value(),
+            lambda self, val: self.s_asgd_bias.set_value(val))
+
+    def __init__(self, n_features,
+            sgd_step_size0=DEFAULT_SGD_STEP_SIZE0,
+            l2_regularization=DEFAULT_L2_REGULARIZATION,
+            n_iterations=DEFAULT_N_ITERATIONS,
+            feedback=DEFAULT_FEEDBACK,
+            rstate=DEFAULT_RSTATE,
+            dtype=DEFAULT_DTYPE,
+            sgd_step_size_scheduling_exponent=DEFAULT_SGD_EXPONENT,
+            sgd_step_size_scheduling_multiplier=DEFAULT_SGD_TIMESCALE):
+
+        BaseASGD.__init__(self,
+            n_features,
+            sgd_step_size0=sgd_step_size0,
+            l2_regularization=l2_regularization,
+            n_iterations=n_iterations,
+            feedback=feedback,
+            rstate=rstate,
+            dtype=dtype,
+            sgd_step_size_scheduling_exponent=sgd_step_size_scheduling_exponent,
+            sgd_step_size_scheduling_multiplier=sgd_step_size_scheduling_multiplier)
+
+        self.s_sgd_weights = theano.shared(
+                np.zeros((n_features), dtype=dtype),
+                name='sgd_weights')
+        self.s_sgd_bias = theano.shared(
+                np.asarray(0, dtype=dtype),
+                name='sgd_bias')
+
+        self.s_asgd_weights = theano.shared(
+                np.zeros((n_features), dtype=dtype),
+                name='asgd_weights')
+        self.s_asgd_bias = theano.shared(
+                np.asarray(0, dtype=dtype),
+                name='asgd_bias')
+
+        self.s_n_observations = theano.shared(np.asarray(0).astype('int64'))
+        del self.n_observations
+
+        self.s_sgd_step_size = theano.shared(np.asarray(0).astype(self.dtype))
+        del self.sgd_step_size
+
+    def vector_updates(self, obs, label):
+        sgd_step_size0 = self.sgd_step_size0
+        sgd_step_size_scheduling_exponent = \
+                self.sgd_step_size_scheduling_exponent
+        sgd_step_size_scheduling_multiplier = \
+                self.sgd_step_size_scheduling_multiplier
+        l2_regularization = self.l2_regularization
+
+        n_observations = self.s_n_observations
+
+        sgd_weights = self.s_sgd_weights
+        sgd_bias = self.s_sgd_bias
+        sgd_step_size = self.s_sgd_step_size
+
+        asgd_weights = self.s_asgd_weights
+        asgd_bias = self.s_asgd_bias
+        asgd_step_size = 1.0 / (n_observations + 1)
+
+        sgd_n = (1 + sgd_step_size0 * n_observations * sgd_step_size_scheduling_multiplier)
+        sgd_step_size = (sgd_step_size0 /
+                (sgd_n ** sgd_step_size_scheduling_exponent))
+
+        margin = label * (tensor.dot(obs, sgd_weights) + sgd_bias)
+        regularized_sgd_weights = sgd_weights * tensor.cast(
+                1 - l2_regularization * sgd_step_size,
+                sgd_weights.dtype)
+
+        new_sgd_weights = tensor.switch(margin < 1,
+                tensor.cast(
+                    regularized_sgd_weights + sgd_step_size * label * obs,
+                    regularized_sgd_weights.dtype),
+                regularized_sgd_weights)
+        new_sgd_bias = tensor.switch(margin < 1,
+                tensor.cast(sgd_bias + sgd_step_size * label,
+                    sgd_bias.dtype),
+                sgd_bias)
+
+        aa = tensor.cast(1 - asgd_step_size, asgd_weights.dtype)
+        bb = tensor.cast(asgd_step_size, asgd_weights.dtype)
+
+        new_asgd_weights = aa * asgd_weights + bb * new_sgd_weights
+        new_asgd_bias = aa * asgd_bias + bb * new_sgd_bias
+
+        new_n_observations = n_observations + 1
+
+        updates = {
+                    sgd_weights: new_sgd_weights,
+                    sgd_bias: new_sgd_bias,
+                    asgd_weights: new_asgd_weights,
+                    asgd_bias: new_asgd_bias,
+                    n_observations: new_n_observations}
+        return updates
+
+    def compile_train_fn_2(self):
+        # This calling strategy is fast, but depends on the use of the CVM
+        # linker.
+        self._tf2_obs = obs = theano.shared(np.zeros((2, 2), dtype=self.dtype))
+        self._tf2_label = label = theano.shared(np.zeros(2, dtype='int64'))
+        self._tf2_idx = idx = theano.shared(np.asarray(0, dtype='int64'))
+        self._tf2_idxmap = idxmap = theano.shared(np.zeros(2, dtype='int64'))
+        updates = self.vector_updates(obs[idxmap[idx]], label[idxmap[idx]])
+        updates[idx] = idx + 1
+        self._train_fn_2 = theano.function([], [],
+                updates=updates,
+                mode=theano.Mode(
+                    optimizer='fast_run',
+                    linker='cvm'))
+
+    def partial_fit(self, X, y):
+        if '_train_fn_2' not in self.__dict__:
+            self.compile_train_fn_2()
+        n_points, n_features = X.shape
+        assert n_features == self.n_features
+        assert (n_points,) == y.shape
+
+        self._tf2_obs.set_value(np.asarray(X, dtype=self.dtype), borrow=True)
+        self._tf2_label.set_value(y, borrow=True)
+        fn = self._train_fn_2.fn
+        self._tf2_idxmap.set_value(np.arange(n_points))
+        self._tf2_idx.set_value(0)
+        for i in xrange(n_points):
+            fn()
+
+    def fit(self, X, y):
+        if '_train_fn_2' not in self.__dict__:
+            self.compile_train_fn_2()
+
+        assert X.ndim == 2
+        assert y.ndim == 1
+
+        n_points, n_features = X.shape
+        assert n_features == self.n_features
+        assert n_points == y.size
+
+        n_iterations = self.n_iterations
+
+        self._tf2_obs.set_value(X, borrow=True)
+        self._tf2_label.set_value(y, borrow=True)
+        fn = self._train_fn_2.fn
+
+        for i in xrange(n_iterations):
+            self._tf2_idxmap.set_value(self.rstate.permutation(n_points))
+            self._tf2_idx.set_value(0)
+            for i in xrange(n_points):
+                fn()
+
+    def decision_function(self, X):
+        return (np.dot(self.s_asgd_weights.get_value(borrow=True), X.T)
+                + self.asgd_bias)
+
+    def predict(self, X):
+        return np.sign(self.decision_function(X))
