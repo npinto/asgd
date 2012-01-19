950a65c2a6035e7a086c01a96b0bed558966aea1
James Bergstra <james.bergstra@gmail.com>
2012-01-19 18:24:15 -0500

    adding utils

---
+++ asgd/utils.py
@@ -0,0 +1,71 @@
+
+import numpy as np
+
+def mean_and_std(X, min_std):
+    # XXX: this loop is more memory efficient than numpy but not as
+    # numerically accurate. It would be better to look at the train_mean,
+    # and then either use the msq for getting unit norms if the train_means
+    # are small-ish, or else use numpy.std if the mean is large enough to
+    # cause numerical trouble
+    m = np.zeros(X.shape[1], dtype='float64')
+    msq = np.zeros(X.shape[1], dtype='float64')
+    for i in xrange(X.shape[0]):
+        alpha = 1.0 / (i + 1)
+        v = X[i]
+        m = (alpha * v) + (1 - alpha) * m
+        msq = (alpha * v * v) + (1 - alpha) * msq
+
+    train_mean = np.asarray(m, dtype=X.dtype)
+    train_std = np.sqrt(np.maximum(
+            msq - m * m,
+            min_std ** 2)).astype(X.dtype)
+    return train_mean, train_std
+
+
+def split_center_normalize(X, y,
+        validset_fraction=.2,
+        validset_max_examples=5000,
+        inplace=False,
+        min_std=1e-4,
+        batchsize=1):
+    n_valid = int(min(
+        validset_max_examples,
+        validset_fraction * X.shape[0]))
+
+    # -- increase n_valid to a multiple of batchsize
+    while n_valid % batchsize:
+        n_valid += 1
+
+    n_train = X.shape[0] - n_valid
+
+    # -- decrease n_train to a multiple of batchsize
+    while n_train % batchsize:
+        n_train -= 1
+
+    if not inplace:
+        X = X.copy()
+
+    train_features = X[:n_train]
+    valid_features = X[n_train:n_train + n_valid]
+    train_labels = y[:n_train]
+    valid_labels = y[n_train:n_train + n_valid]
+
+    train_mean, train_std = mean_and_std(X, min_std=min_std)
+
+    # train features and valid features are aliased to X
+    X -= train_mean
+    X /= train_std
+
+    return ((train_features, train_labels),
+            (valid_features, valid_labels),
+            train_mean,
+            train_std)
+
+
+def simple_bracket_min(f, pt0, pt1):
+    v0 = f(pt0)
+    v1 = f(pt1)
+    if v0 > v1:
+        while v0 > v1:
+            raise NotImplementedError()
+
