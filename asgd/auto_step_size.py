a373a45c8f61f4f37c5fc91c7d1ee102e7f7ae3c
Nicolas Pinto <pinto@alum.mit.edu>
2012-01-30 22:18:24 -0500

    ENH: merge jaberg/auto_step_size with cosmetic changes

--- asgd/auto_step_size.py
+++ asgd/auto_step_size.py
@@ -1,43 +1,58 @@
-"""
-"""
 import copy
 import numpy as np
-import scipy.optimize
+from scipy import optimize
 
-DEFAULT_INITIAL_STEPSIZES = 0.25, 0.5
+DEFAULT_INITIAL_RANGE = 0.25, 0.5
 DEFAULT_MAX_EXAMPLES = 1000
-DEFAULT_TOL = 0.5
+DEFAULT_TOLERANCE = 0.5
+DEFAULT_BRENT_OUTPUT = False
 
-def binary_sgd_best_step_size(model, X, y,
-        initial_stepsizes=DEFAULT_INITIAL_STEPSIZES,
-        tol=DEFAULT_TOL,
-        full_output=False):
+
+def find_sgd_step_size0(
+    model, X, y,
+    initial_range=DEFAULT_INITIAL_RANGE,
+    tolerance=DEFAULT_TOLERANCE, brent_output=DEFAULT_BRENT_OUTPUT):
     """Use a Brent line search to find the best step size
 
-    model - a BinaryASGD instance
-    X - features
-    y - labels in (-1, 1)
-    initial_stepsize - (low, high) start stepsize search here
-    maxiter - higher number -> more accurate minimization
-    full_output - return all outputs from scipy.optimize.brent (DEBUG)
+    Parameters
+    ----------
+    model: BinaryASGD
+        Instance of a BinaryASGD
 
-    Returns: optimal stepsize for given X, y data.
+    X: array, shape = [n_samples, n_features]
+        Array of features
+
+    y: array, shape = [n_samples]
+        Array of labels in (-1, 1)
+
+    initial_range: tuple of float
+        Initial range for the sgd_step_size0 search (low, high)
+
+    max_iterations:
+        Maximum number of interations
+
+    Returns
+    -------
+    best_sgd_step_size0: float
+        Optimal sgd_step_size0 given `X` and `y`.
     """
     # -- stupid scipy calls some sizes twice!?
     _cache = {}
+
     def eval_size0(log2_size0):
         try:
             return _cache[log2_size0]
         except KeyError:
             pass
         other = copy.deepcopy(model)
-        other.sgd_step_size0 = 2 ** log2_size0
-        other.sgd_step_size = 2 ** log2_size0
+        current_step_size = 2 ** log2_size0
+        other.sgd_step_size0 = current_step_size
+        other.sgd_step_size = current_step_size
         other.partial_fit(X, y)
-        # Hack: asgd is lower variance than sgd, but it's tuned to work well
-        # asymptotically, not after just a few examples
-        weights = .5 * other.asgd_weights + .5 * other.sgd_weights
-        bias = .5 * other.asgd_bias + .5 * other.sgd_bias
+        # Hack: asgd is lower variance than sgd, but it's tuned to work
+        # well asymptotically, not after just a few examples
+        weights = .5 * (other.asgd_weights + other.sgd_weights)
+        bias = .5 * (other.asgd_bias + other.sgd_bias)
 
         margin = y * (np.dot(X, weights) + bias)
         l2_cost = other.l2_regularization * (weights ** 2).sum()
@@ -45,41 +60,57 @@ def binary_sgd_best_step_size(model, X, y,
         _cache[log2_size0] = rval
         return rval
 
-    xbest = scipy.optimize.brent(
-            eval_size0,
-            brack=np.log2(initial_stepsizes),
-            tol=tol,
-            full_output=full_output)
-    return xbest
+    best_sgd_step_size0 = optimize.brent(
+        eval_size0, brack=np.log2(initial_range), tol=tolerance)
+
+    return best_sgd_step_size0
 
 
-def binary_fit(model, X, y,
-        max_examples=DEFAULT_MAX_EXAMPLES,
-        **binary_sgd_best_step_size_kwargs):
-    """Returns a model with automatically-selected stepsize
+def binary_fit(
+    model, X, y,
+    max_examples=DEFAULT_MAX_EXAMPLES,
+    **find_sgd_step_size0_kwargs):
+    """Returns a model with automatically-selected sgd_step_size0
 
-    model - a BinaryASGD instance
-    X - features
-    y - labels in (-1, 1)
-    max_examples - estimate the stepsize from at most this many examples
-    initial_stepsize - (low, high) start stepsize search here
+    Parameters
+    ----------
+    model: BinaryASGD
+        Instance of the model to be fitted.
 
+    X: array, shape = [n_samples, n_features]
+        Array of features
+
+    y: array, shape = [n_samples]
+        Array of labels in (-1, 1)
+
+    max_examples: int
+        Maximum number of examples to use from `X` and `y` to find an
+        estimate of the best sgd_step_size0
+
+    Returns
+    -------
+    model: BinaryASGD
+        Instances of the model, fitted with an estimate of the best
+        sgd_step_size0
     """
+
+    assert X.ndim == 2
+    assert len(X) == len(y)
+    assert max_examples > 0
+
     # randomly choose up to max_examples uniformly without replacement from
     # across the whole set of training data.
     idxs = model.rstate.permutation(len(X))[:max_examples]
 
     # Find the best learning rate for that subset
-    best = binary_sgd_best_step_size(
-            model,
-            X[idxs],
-            y[idxs],
-            **binary_sgd_best_step_size_kwargs)
+    best = find_sgd_step_size0(
+        model, X[idxs], y[idxs], **find_sgd_step_size0_kwargs)
 
     # Heuristic: take the best stepsize according to the first max_examples,
     # and go half that fast for the full run.
-    model.sgd_step_size0 = 2 ** (best - 1.0)
-    model.sgd_step_size = 2 ** (best - 1.0)
+    best_estimate = 2. ** (best - 1.0)
+    model.sgd_step_size0 = best_estimate
+    model.sgd_step_size = best_estimate
     model.fit(X, y)
-    return model
 
+    return model
--- asgd/tests/test_auto_step_size.py
+++ asgd/tests/test_auto_step_size.py
@@ -1,83 +1,77 @@
-import numpy as np
-from numpy.random import RandomState
+from nose.tools import assert_equal
+from numpy.testing import assert_almost_equal
+from numpy.testing import assert_array_equal
 
+from numpy.random import RandomState
 from asgd import NaiveBinaryASGD as BinaryASGD
-from asgd.auto_step_size import binary_sgd_best_step_size
+from asgd.auto_step_size import find_sgd_step_size0
 from asgd.auto_step_size import binary_fit
 from asgd.auto_step_size import DEFAULT_MAX_EXAMPLES
 
 from test_naive_asgd import get_fake_data
 
 
-def test_binary_sgd_best_step_size():
+def get_new_model(n_features, rstate):
+    return BinaryASGD(n_features, rstate=rstate,
+            sgd_step_size0=1e3,
+            l2_regularization=1e-3,
+            n_iterations=5)
+
+
+def test_binary_sgd_step_size0():
     rstate = RandomState(42)
-    N_FEATURES = 20
+    n_features = 20
 
-    X, y = get_fake_data(100, N_FEATURES, rstate)
+    X, y = get_fake_data(100, n_features, rstate)
 
-    def new_model():
-        return BinaryASGD(N_FEATURES, rstate=rstate,
-                sgd_step_size0=1000.0,
-                l2_regularization=1e-3,
-                n_iterations=5)
-
-    clf = new_model()
-    x, fx, ni, nf = binary_sgd_best_step_size(clf, X, y, (.25, .5), full_output=True)
-
-    print x, fx, ni, nf
-    assert nf < 10
-    assert abs(x + 5) < .5
+    clf = get_new_model(n_features, rstate)
+    best = find_sgd_step_size0(clf, X, y, (.25, .5))
+    assert_almost_equal(best, -4.9927, decimal=4)
 
     # start a little lower, still works
-    x, fx, ni, nf = binary_sgd_best_step_size(clf, X, y, (.125, .25), full_output=True)
-    assert nf < 10
-    assert abs(x + 5) < .5
+    best = find_sgd_step_size0(clf, X, y, (.125, .25))
+    assert_almost_equal(best, -4.6180, decimal=4)
 
-    # binary_sgd_best_step_size does not change clf
+    # find_sgd_step_size0 does not change clf
     assert clf.sgd_step_size0 == 1000.0
 
 
 def test_binary_fit():
     rstate = RandomState(42)
-    N_FEATURES = 20
+    n_features = 20
 
-    def new_model():
-        return BinaryASGD(N_FEATURES, rstate=rstate,
-                sgd_step_size0=1000.0,
-                l2_regularization=1e-3,
-                n_iterations=5)
-
-    clf100 = new_model()
-    X, y = get_fake_data(100, N_FEATURES, rstate)
+    clf100 = get_new_model(n_features, rstate)
+    X, y = get_fake_data(100, n_features, rstate)
     _clf100 = binary_fit(clf100, X, y)
     assert _clf100 is clf100
-    assert clf100.sgd_step_size0 != 1000.0
+    assert_almost_equal(clf100.sgd_step_size0, 0.04812, decimal=4)
 
     # smoke test
-    clf1000 = new_model()
-    X, y = get_fake_data(DEFAULT_MAX_EXAMPLES, N_FEATURES, rstate)
+    clf1000 = get_new_model(n_features, rstate)
+    X, y = get_fake_data(DEFAULT_MAX_EXAMPLES, n_features, rstate)
     _clf1000 = binary_fit(clf1000, X, y)
     assert _clf1000 is clf1000
-    assert clf100.sgd_step_size0 != clf1000.sgd_step_size0
+    assert_almost_equal(clf1000.sgd_step_size0, 0.0047, decimal=4)
 
     # smoke test that at least it runs
-    clf2000 = new_model()
-    X, y = get_fake_data(2000, N_FEATURES, rstate)
+    clf2000 = get_new_model(n_features, rstate)
+    X, y = get_fake_data(2000, n_features, rstate)
     _clf2000 = binary_fit(clf2000, X, y)
+    assert _clf2000 == clf2000
+    assert_almost_equal(clf2000.sgd_step_size0, 0.0067, decimal=4)
 
 
 def test_fit_replicable():
 
-    N_FEATURES = 20
+    n_features = 20
 
-    def new_model():
-        return BinaryASGD(N_FEATURES, rstate=RandomState(45),
-                sgd_step_size0=1000.0,
-                l2_regularization=1e-3,
-                n_iterations=5)
+    X, y = get_fake_data(100, n_features, RandomState(4))
 
-    X, y = get_fake_data(100, N_FEATURES, RandomState(4))
-    m0 = binary_fit(new_model(), X, y)
-    m1 = binary_fit(new_model(), X, y)
-    assert np.all(m0.sgd_weights == m1.sgd_weights)
-    assert np.all(m0.asgd_bias == m1.asgd_bias)
+    m0 = get_new_model(n_features, RandomState(45))
+    m0 = binary_fit(m0, X, y)
+
+    m1 = get_new_model(n_features, RandomState(45))
+    m1 = binary_fit(m1, X, y)
+
+    assert_array_equal(m0.sgd_weights, m1.sgd_weights)
+    assert_array_equal(m0.sgd_bias, m1.sgd_bias)
